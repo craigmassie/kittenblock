1b3c172816420e9222409b8c3e4760f2ab27260d
Craig Massie <craigsmassie@gmail.com>
2020-05-10 22:48:03 +0100

    Added The Sun block

--- kblock.js
+++ kblock.js
@@ -1,11 +1,15 @@
 
-function checkforDM (tabId, changeInfo, tab) {
-    if (tab.url.indexOf('dailymail.co.uk') > -1) {
+function checkForTabloid(tabId, changeInfo, tab) {
+    if (tab.url.indexOf('www.dailymail.co.uk') > -1){
         chrome.tabs.update(tabId, {url: "http://www.teaandkittens.co.uk/blocked/"});	  
     };
-    if (tab.url.indexOf('www.express.co.uk') > -1) {
+    if (tab.url.indexOf('www.express.co.uk') > -1){
+        chrome.tabs.update(tabId, {url: "http://www.teaandkittens.co.uk/blocked/"});	  
+    };
+    if (tab.url.indexOf('www.thesun.co.uk') > -1){
         chrome.tabs.update(tabId, {url: "http://www.teaandkittens.co.uk/blocked/"});	  
     };
 };
+
 // Listen
-chrome.tabs.onUpdated.addListener(checkforDM);
+chrome.tabs.onUpdated.addListener(checkForTabloid);
